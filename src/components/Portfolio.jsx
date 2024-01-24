0582c26d620e03522ba25550923cd54dec1df6c9
kolamat <matanmidupe004@gmail.com>
2024-01-24 16:53:07 +0100

    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   node_modules/.cache/default-development/0.pack
    	modified:   node_modules/.cache/default-development/index.pack
    	modified:   src/components/Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -13,28 +13,16 @@ function Portfolio() {
         Latests <span className="text-[#0ef]">Project</span>
       </h2>
       <div className="grid auto-cols-auto md:grid-cols-3 sm:grid-cols-2 mx-auto gap-5 box-border py-11 px-4">
+        <img className="mx-auto my-4 rounded-2xl" src={mokLogo} alt="Kolade" />
+        <img className="mx-auto my-4 rounded-2xl" src={Mok} alt="Kolade" />
         <img
-          className="mx-auto my-4 rounded-2xl w-[100%] h-[90%]"
-          src={mokLogo}
-          alt="Kolade"
-        />
-        <img
-          className="mx-auto my-4 rounded-2xl w-[100%] h-[90%]"
-          src={Mok}
-          alt="Kolade"
-        />
-        <img
-          className="mx-auto my-4 rounded-2xl w-[100%] h-[90%]"
+          className="mx-auto my-4 rounded-2xl"
           src={twoIphones}
           alt="Kolade"
         />
+        <img className="mx-auto my-4 rounded-2xl" src={mokLogo2} alt="Kolade" />
         <img
-          className="mx-auto my-4 rounded-2xl w-[100%] h-[90%]"
-          src={mokLogo2}
-          alt="Kolade"
-        />
-        <img
-          className="mx-auto my-4 rounded-2xl w-[100%] h-[90%]"
+          className="mx-auto my-4 rounded-2xl"
           src={laptopCard}
           alt="Kolade"
         />
