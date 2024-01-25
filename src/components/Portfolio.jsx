c03263aa2872beecaa41111105da8c800eebdc76
kolamat <matanmidupe004@gmail.com>
2024-01-25 14:57:12 +0100

    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   node_modules/.cache/default-development/0.pack
    	modified:   node_modules/.cache/default-development/index.pack
    	modified:   src/components/Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,37 +12,17 @@ function Portfolio() {
       <h2 className="text-white text-[2.8rem] text-center font-bold py-1">
         Latests <span className="text-[#0ef]">Project</span>
       </h2>
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 mx-auto gap-5 box-border py-11 px-4 object-contain">
-        <img
-          className="mx-auto rounded-2xl h-96 w-96"
-          src={mokLogo}
-          alt="Kolade"
-        />
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 mx-auto gap-5 box-border py-11 px-4 place-items-center">
+        <img className=" rounded-2xl h-96 w-96" src={mokLogo} alt="Kolade" />
         {/* <img
-          className="mx-auto rounded-2xl h-96 w-96"
+          className=" rounded-2xl h-96 w-96"
           src={Mok}
           alt="Kolade"
         /> */}
-        <img
-          className="mx-auto rounded-2xl h-96 w-96"
-          src={twoIphones}
-          alt="Kolade"
-        />
-        <img
-          className="mx-auto rounded-2xl h-96 w-96"
-          src={mokLogo2}
-          alt="Kolade"
-        />
-        <img
-          className="mx-auto rounded-2xl h-96 w-96"
-          src={laptopCard}
-          alt="Kolade"
-        />
-        <img
-          className="mx-auto rounded-2xl h-96 w-96"
-          src={makeUp}
-          alt="Kolade"
-        />
+        <img className=" rounded-2xl h-96 w-96" src={twoIphones} alt="Kolade" />
+        <img className=" rounded-2xl h-96 w-96" src={mokLogo2} alt="Kolade" />
+        <img className=" rounded-2xl h-96 w-96" src={laptopCard} alt="Kolade" />
+        <img className=" rounded-2xl h-96 w-96" src={makeUp} alt="Kolade" />
       </div>
     </div>
   );
