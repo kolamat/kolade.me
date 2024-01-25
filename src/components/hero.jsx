c132f94e9e33c5fbfa4f71e5c62695db6fcee03b
kolamat <matanmidupe004@gmail.com>
2024-01-25 19:02:37 +0100

    On branch gh-pages Your branch is up to date with 'origin/gh-pages'.
    Changes to be committed:
    	modified:   src/components/hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,35 +1,3 @@
-# Prerequisites
-*.d
-
-# Compiled Object files
-*.slo
-*.lo
-*.o
-*.obj
-
-# Precompiled Headers
-*.gch
-*.pch
-
-# Compiled Dynamic libraries
-*.so
-*.dylib
-*.dll
-
-# Fortran module files
-*.mod
-*.smod
-
-# Compiled Static libraries
-*.lai
-*.la
-*.a
-*.lib
-
-# Executables
-*.exe
-*.out
-*.app
 import React from "react";
 import Typed from "react-typed";
 import { FaFacebook } from "react-icons/fa";
