9b56c761a40a62f96319da388ef6813a1f08aba5
kolamat <matanmidupe004@gmail.com>
2024-01-27 16:32:36 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/components/Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -11,7 +11,7 @@ function Services() {
       </h1>
       {/* mx-auto grid md:grid-cols-3 gap-8 items-center */}
       <div className="grid grid-cols-auto lg:grid-cols-3 gap-4">
-        <div className="flex flex-col items-center border p-4  justify-between rounded-[15px] w-[100%] hover:border-sky-400 hover:ease-in-out">
+        <div className="flex flex-col items-center border p-4  justify-between rounded-[15px] w-[100%] hover:border-sky-400 hover:ease-linear">
           <h3 className="flex flex-col items-center justify-center text-[27px] font-bold">
             <FaCode size={42} />
             Web Developer
