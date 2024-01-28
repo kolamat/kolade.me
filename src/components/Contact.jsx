ecb09a3926d4f325c37033984090db0000611347
kolamat <matanmidupe004@gmail.com>
2024-01-28 08:03:33 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/components/Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,56 +1,24 @@
 import React from "react";
 
 function Contact() {
+       emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <div className="grid">
-      <div className="px-4">
-        <h2 className="text-white text-[2.8rem] text-center font-bold py-1">
-          Contact <span className="text-[#0ef]">Me</span>
-        </h2>
-
-        <div className="flex flex-col md:grid-cols-3 sm:flex-col">
-          <form>
-            <div className="grid grid-cols-2 gap-1  lg:flex sm:flex-col">
-              <input
-                type="text"
-                placeholder="K*****"
-                className="bg-gray-600 text-white rounded-lg"
-              />
-              <input
-                type="email"
-                placeholder="Email: M***********"
-                className="bg-gray-600 text-white rounded-lg"
-              />
-            </div>
-
-            <div className="grid grid-cols-2 gap-1 my-2 text-center">
-              <input
-                type="number"
-                placeholder="Num:+234**********"
-                className="bg-gray-600 text-white rounded-lg"
-              />
-              <input
-                type="text"
-                placeholder="Email Subject"
-                className="bg-gray-600 text-white rounded-lg"
-              />
-            </div>
-            <textarea
-              name=""
-              id=""
-              cols="30"
-              rows="10"
-              placeholder="Your Message"
-              className="bg-gray-600 w-80 text-white rounded-xl"
-            ></textarea>
-          </form>
-          <input
-            type="submit"
-            value="Send Message"
-            className="bg-gray-600 mt-4 mb-8 rounded-xl text-white w-56 m-auto"
-          />
-        </div>
-      </div>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
     </div>
   );
 }
