b4877e47481f997212735c3ba52c04954d610530
kolamat <matanmidupe004@gmail.com>
2024-01-28 10:11:42 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/components/Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,13 @@ export const Contact = () => {
       <div className="grid md:flex">
         <form ref={form} onSubmit={sendEmail} className=" md:items-start">
           <label>Name</label>
-          <input type="text" name="user_name" />
+          <input type="text" name="user_name" placeholder="K*****" />
           <label>Email</label>
-          <input type="email" name="user_email" />
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Email: M***********"
+          />
           <label>Message</label>
           <textarea name="message" />
           <input type="submit" value="Send" />
