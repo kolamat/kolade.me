44cbfc2dce1a964a73f4fd7d9b4690dd4d568f1f
kolamat <matanmidupe004@gmail.com>
2024-01-28 09:11:02 +0100

    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   node_modules/.cache/default-development/index.pack
    	modified:   src/components/Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 // import React from "react";
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
-
+import kolade from "../assets/kolade.jpg";
 import style from "./app.css";
 
 export const Contact = () => {
