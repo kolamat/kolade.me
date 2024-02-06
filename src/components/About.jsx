7268a4a737eae83603b0c32d74f4dbbd7a8090bd
kolamat <matanmidupe004@gmail.com>
2024-02-06 17:28:09 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/components/About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,7 @@ function about() {
           </p>
           <a
             href="/"
-            className=" font-semibold w-[98px] rounded-lg bg-[#0ef] p-2 text-center text-black hover:text-white hover:bg-slate-900 hover:bg-opacity-100 hover:translate-x-2 ease-in-out hover:transition-transform"
+            className=" font-semibold w-[98px] rounded-lg bg-[#0ef] p-2 text-center text-black hover:text-white hover:bg-slate-900 hover:bg-opacity-100 hover:translate-x-2 ease-in-out hover:transition-[0.5s,ease]"
           >
             Read More
           </a>
