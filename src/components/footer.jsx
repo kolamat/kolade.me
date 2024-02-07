7189a69f64e616aaaf824de4274d95eec57a801c
kolamat <matanmidupe004@gmail.com>
2024-02-07 23:46:47 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/components/footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,20 +4,7 @@ import React from "react";
 function footer() {
   return (
     <div classname="w-full">
-      <div className="h-1 bg-[#0ef] rounded-sm flex">
-        <span className="bg-[#fff] w-"></span>
-        <span className="bg-[#000] w-"></span>
-        <span className="bg-[#fff] w-"></span>
-        <span className="bg-[#000] w-"></span>
-        <span className="bg-[#fff] w-"></span>
-        <span className="bg-[#000] w-"></span>
-        <span className="bg-[#fff] w-"></span>
-        <span className="bg-[#000] w-"></span>
-        <span className="bg-[#fff] w-"></span>
-        <span className="bg-[#000] w-"></span>
-        <span className="bg-[#fff] w-"></span>
-        <span className="bg-[#000] w-"></span>
-      </div>
+      <div className="h-1 bg-[#0ef] rounded-sm flex"></div>
       <div className=" text-white flex justify-between px-5 py-2 tracking-wide">
         <div>
           <p className="block">
