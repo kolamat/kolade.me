920d88e1a42d5d60f8424518e1d40ac2fb8155f3
kolamat <matanmidupe004@gmail.com>
2024-03-01 16:37:29 +0100

    On branch main Your branch is ahead of 'origin/main' by 30 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -16,7 +16,7 @@ function footer() {
                 width="50"
                 height="28"
                 viewBox="0 0 94 108"
-                fill="none"
+                fill="#E1986F"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
