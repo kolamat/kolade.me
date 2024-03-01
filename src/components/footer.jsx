54ca0d8dd9daf7387be59d52d41fbb112844f3bd
kolamat <matanmidupe004@gmail.com>
2024-03-01 16:44:51 +0100

    Changes to be committed: 	modified:   src/components/footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,7 +12,7 @@ function footer() {
             {/* <img src={logo} alt="logo" /> */}
             <a href="/">
               <svg
-                className="hover:stroke-[#FFC132] transition duration-500 mt-2"
+                className="hover:stroke-[#FFC132] mt-2"
                 width="50"
                 height="28"
                 viewBox="0 0 94 108"
@@ -20,7 +20,6 @@ function footer() {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  className="transition duration-500"
                   d="M54.0434 101.423L55.3836 102.908H57.384H72.616H82.8456L75.9332 95.3673L37.8715 53.8453L75.5678 12.2124L82.377 4.69202H72.232H57.512H55.4954L54.1533 6.19722L25.148 38.7274V9.19202V4.69202H20.648H9H4.5V9.19202V98.408V102.908H9H20.648H25.148V98.408V69.4065L54.0434 101.423Z"
                   //   stroke="#"
                   stroke-width="9"
