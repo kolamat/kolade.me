c7da7d51abc6c02454b062f7c6c41b515a3b3804
kolamat <matanmidupe004@gmail.com>
2024-03-01 19:29:56 +0100

    On branch main Your branch is ahead of 'origin/main' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/portfolio/Portfolio.jsx

--- src/portfolio/Portfolio.jsx
+++ src/portfolio/Portfolio.jsx
@@ -15,28 +15,32 @@ function Portfolio() {
       </h2>
       <div className="relative grid lg:grid-cols-3 md:grid-cols-2 mx-6 gap-5 box-border py-11 px-4 place-items-center">
         <p>
-          <img className=" rounded-2xl h-96 w-" src={mokLogo} alt="Kolade" />
+          <img
+            className=" rounded-2xl h-96 w-96 cursor-pointer"
+            src={mokLogo}
+            alt="Kolade"
+          />
         </p>
         <img
-          className=" rounded-2xl h-96 w-"
+          className=" rounded-2xl h-96 w-96"
           src={twoIphones}
           alt="Kolade"
           loading="lazy"
         />
         <img
-          className=" rounded-2xl h-96 w-"
+          className=" rounded-2xl h-96 w-96"
           src={mokLogo2}
           alt="Kolade"
           loading="lazy"
         />
         <img
-          className=" rounded-2xl h-96 w-"
+          className=" rounded-2xl h-96 w-96"
           src={laptopCard}
           alt="Kolade"
           loading="lazy"
         />
         <img
-          className=" rounded-2xl h-96 w-"
+          className=" rounded-2xl h-96 w-96"
           src={makeUp}
           alt="Kolade"
           loading="lazy"
