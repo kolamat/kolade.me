b78e5ef89777eefe8dae2c660c5d1b2ca39acdf2
kolamat <matanmidupe004@gmail.com>
2024-03-01 16:27:02 +0100

    On branch main Your branch is ahead of 'origin/main' by 25 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ import { FaLinkedin, FaXTwitter } from "react-icons/fa6";
 
 function footer() {
   return (
-    <section className="mt- mb-14 mx-auto">
+    <section className="mb-14">
       <div className="h-[1px] bg-[#0ef] rounded-sm flex justify-center" />
       <div className=" text-white flex justify-between sm:px-5 py-2 tracking-wide">
         <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
