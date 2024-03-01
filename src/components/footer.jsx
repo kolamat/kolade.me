821501a46c09f9b40e5d877ed60b67ca83f3640a
kolamat <matanmidupe004@gmail.com>
2024-03-01 16:16:20 +0100

    On branch main Your branch is ahead of 'origin/main' by 19 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -42,7 +42,7 @@ function footer() {
           </div>
           <div className="md:grid text-center items- mt-16 basis-1/4 grid grid-cols-1 md:mt-4">
             <h4 className="font-bold">Links</h4>
-            <div className="flex gap- md:gap-0 md:flex-col col-span-2 items-center">
+            <div className="flex gap-7 md:gap-0 md:flex-col col-span-2 items-center">
               <p className="my-2">
                 <a
                   className="rounded-full bg-[#0ef] text-black hover:text-white hover:bg-slate-900 hover:transition-transform ease-in-out hover:bg-opacity-100 hover:translate-x-1"
