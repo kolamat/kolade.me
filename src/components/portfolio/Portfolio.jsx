d8e2708441c68904ef9f5f7b06774390deb39cf6
kolamat <matanmidupe004@gmail.com>
2024-03-02 10:00:38 +0100

    On branch main Your branch is ahead of 'origin/main' by 15 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/portfolio/Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -10,37 +10,38 @@ function Portfolio() {
       id="portfolio"
       className="w-full md:items-center m- text-white py-[7rem] md:px-4 bg-[#000300]"
     >
-      <h2 className="text-white text-[2.8rem] text-center font-bold py-1">
-        Latests <span className="text-[#0ef] text-center">Project</span>
+      <h2 className="text-white md:text-[2.8rem] text-[31px] text-center font-bold py-1">
+        Latests{" "}
+        <span className="text-[#0ef] text-center underline">Project</span>
       </h2>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 mx-6 gap-5 box-border py-11 px-4 place-items-center">
         <p>
           <img
-            className=" rounded-2xl h-96 w-96 cursor-pointer"
+            className=" rounded-2xl w-[310px] h-72 lg:w-[500px] cursor-pointer"
             src={mokLogo}
             alt="Kolade"
           />
         </p>
         <img
-          className=" rounded-2xl h-96 w-96"
+          className="rounded-2xl w-[310px] h-72 lg:w-[500px]"
           src={twoIphones}
           alt="Kolade"
           loading="lazy"
         />
         <img
-          className=" rounded-2xl h-96 w-96"
+          className=" rounded-2xl w-[310px] h-72 lg:w-[500px]"
           src={mokLogo2}
           alt="Kolade"
           loading="lazy"
         />
         <img
-          className=" rounded-2xl h-96 w-96"
+          className=" rounded-2xl w-[310px] h-72 lg:w-[500px]"
           src={laptopCard}
           alt="Kolade"
           loading="lazy"
         />
         <img
-          className=" rounded-2xl h-96 w-96"
+          className=" rounded-2xl w-[310px] h-72 lg:w-[500px]"
           src={makeUp}
           alt="Kolade"
           loading="lazy"
