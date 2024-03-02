4eb6f448aa29c9e955abe78785c74cf2dd93a74b
kolamat <matanmidupe004@gmail.com>
2024-03-02 09:44:03 +0100

    On branch main Your branch is ahead of 'origin/main' by 14 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/App.js
    	renamed:    src/components/contact/footer/footer.jsx -> src/components/footer/footer.jsx

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from "./components/about/About";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
-import Footer from "./components/contact/footer/footer";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
