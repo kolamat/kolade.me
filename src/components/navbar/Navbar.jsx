744565fe52acd97eeef8017f1f539249164643be
kolamat <matanmidupe004@gmail.com>
2024-03-02 08:06:33 +0100

    On branch main Your branch is ahead of 'origin/main' by 6 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/navbar/Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navbar active">
+    <nav className="navbar active">
       <a href="/" className="flex">
         <div className="flex items-center justify-between gap-1 sm:ml-6 md:pl-4">
           <svg
@@ -96,7 +96,7 @@ const Navbar = () => {
           </a>
         </ul>
       </div>
-    </div>
+    </nav>
   );
 };
 
