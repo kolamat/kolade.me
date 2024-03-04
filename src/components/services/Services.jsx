6e6a12be39d75c3cccf25cfd8a3b3c21482ffa75
kolamat <matanmidupe004@gmail.com>
2024-03-04 16:33:45 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/components/services/Services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -49,7 +49,7 @@ function Services() {
             traditional Marketing and is often considered an additional way for
             companies to approach consumers and understand their behavior.
           </p>
-          <button className="font-semibold w-[98px] rounded-lg bg-[#0ef] p-2 text-center text-black hover:text-white hover:bg-slate-900 bg-opacity-100 hover:translate-y-2 transition duration-300 delay-75">
+          <button className="font-semibold w-[98px] rounded-lg bg-[#0ef] p-2 text-center text-black hover:text-white hover:bg-slate-900 hover:bg-opacity-100 hover:translate-y-2 transition duration-300 delay-75">
             <a href="/">Read More</a>
           </button>
         </div>
