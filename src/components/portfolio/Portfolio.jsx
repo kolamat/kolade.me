6bb224e6f1760ecd822de672c016312e11065e39
kolamat <matanmidupe004@gmail.com>
2024-03-05 14:24:27 +0100

    On branch main Your branch is ahead of 'origin/main' by 3 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/portfolio/Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -16,7 +16,7 @@ function Portfolio() {
         Latests{" "}
         <span className="text-[#60a5fa] text-center underline">Project</span>
       </h2>
-      <div className="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 mx-6 gap-2 box-border py-11 px-4 place-items-center">
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 mx-6 gap-2 box-border py-11 px-4 place-items-center">
         <p>
           <img className={`${imageStyles}`} src={mokLogo} alt="Kolade" />
         </p>
@@ -44,6 +44,22 @@ function Portfolio() {
           alt="Kolade"
           loading="lazy"
         />
+        <div>
+          <svg
+            className="hover:fill-[#60a5fa] hover:stroke-[#E1986F] transition duration-300"
+            width="250"
+            height="300"
+            viewBox="0 0 94 108"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M54.0434 101.423L55.3836 102.908H57.384H72.616H82.8456L75.9332 95.3673L37.8715 53.8453L75.5678 12.2124L82.377 4.69202H72.232H57.512H55.4954L54.1533 6.19722L25.148 38.7274V9.19202V4.69202H20.648H9H4.5V9.19202V98.408V102.908H9H20.648H25.148V98.408V69.4065L54.0434 101.423Z"
+              stroke="#fff"
+              stroke-width="9"
+            />
+          </svg>
+        </div>
       </div>
     </div>
   );
