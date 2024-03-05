906ec27aebda4646c1f5663d6a26c05ed01d56dd
kolamat <matanmidupe004@gmail.com>
2024-03-05 10:24:58 +0100

    On branch main Your branch is ahead of 'origin/main' by 6 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/navbar/Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
     "p-4 border-b border-gray-600 hover:text-[#147bf9] hover:font-extrabold hover:underline ease-in duration-200";
 
   const navDesktop =
-    "p-4 hover:text-[#147bf9] hover:font-extrabold hover:underline ease-in duration";
+    "p-4 hover:text-[#147bf9] hover:font-extrabold hover:underline ease-in duration-200";
 
   return (
     <nav className={navbar ? "navbar active" : "navbar"}>
