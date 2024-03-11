8aaa11d0c517c05f47e875227a17f79f063ec0b2
kolamat <matanmidupe004@gmail.com>
2024-03-11 04:52:56 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 3 commits.
      (use "git push" to publish your local commits)

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -43,20 +43,6 @@ function Portfolio() {
           loading="lazy"
         />
         <div className="relative inline-block">
-          <p
-            className="p-5 absolute z-30 flex
-    h-full w-full items-center justify-center flex-col
-    whitespace-normal text-center text-[#6a60fa]
-    opacity-0 transition duration-500 hover:opacity-90 font-bold text-2xl bg-black rounded-lg translate-y-5"
-          >
-            <span className="underline"> Logo_Design</span>
-            <a
-              className="font-medium text-lg text-[#60a8fa] no-underline hover:underline"
-              href="/"
-            >
-              Contact for your business-logo designs
-            </a>
-          </p>
           <svg
             // className="hover:fill-[#60a5fa] hover:stroke-[#E1986F] transition duration-300 hover:blur-sm"
             width="250"
@@ -71,6 +57,20 @@ function Portfolio() {
               stroke-width="9"
             />
           </svg>
+          <p
+            className="p-5 absolute z-30 flex
+    h-full w-full items-center justify-center flex-col
+    whitespace-normal text-center text-[#6a60fa]
+    opacity-0 transition duration-500 hover:opacity-90 font-bold text-2xl bg-black rounded-lg translate-y-5"
+          >
+            <span className="underline"> Logo_Design</span>
+            <a
+              className="font-medium text-lg text-[#60a8fa] no-underline hover:underline"
+              href="/"
+            >
+              Contact for your business-logo designs
+            </a>
+          </p>
         </div>
       </div>
     </div>
