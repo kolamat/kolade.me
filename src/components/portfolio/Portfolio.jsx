4bcce5f3a9ca2adc4621f61f9a4fbb2726755c92
kolamat <matanmidupe004@gmail.com>
2024-03-11 04:43:24 -0300

    On branch main Your branch is ahead of 'origin/main' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/portfolio/Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -47,7 +47,7 @@ function Portfolio() {
             className="p-5 absolute z-30 flex
     h-full w-full items-center justify-center flex-col
     whitespace-normal text-center text-[#6a60fa]
-    opacity-0 transition duration-500 hover:opacity-90 font-bold text-2xl bg-black rounded-lg"
+    opacity-0 transition duration-500 hover:opacity-90 font-bold text-2xl bg-black rounded-lg translate-y-5"
           >
             <span className="underline"> Logo_Design</span>
             <a
