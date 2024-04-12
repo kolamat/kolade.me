1039b9fbc87d293aac8959d5d044de43118c4bc0
kolamat <matanmidupe004@gmail.com>
2024-04-12 19:35:55 +0100

    On branch main Your branch is ahead of 'origin/main' by 8 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/portfolio/Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,8 +3,6 @@ import makeUp from "../../assets/Makeup.PNG";
 import mokLogo from "../../assets/mok-logo.PNG";
 import mokLogo2 from "../../assets/mok-logo2.PNG";
 import twoIphones from "../../assets/two-iphones.PNG";
-import kDesign from "../../assets/k-design.svg";
-// import Image from "next/image";
 
 const imageStyles = "rounded-2xl w-[310px] h-[300px] lg:w-[300px]";
 
@@ -59,7 +57,20 @@ function Portfolio() {
               Contact for your business-logo designs
             </a>
           </p>
-          <img src={kDesign} alt="k-logo" width={40} height={1} />
+          <svg
+            // className="hover:fill-[#60a5fa] hover:stroke-[#E1986F] transition duration-300 hover:blur-sm"
+            width="250"
+            height="300"
+            viewBox="0 0 94 108"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M54.0434 101.423L55.3836 102.908H57.384H72.616H82.8456L75.9332 95.3673L37.8715 53.8453L75.5678 12.2124L82.377 4.69202H72.232H57.512H55.4954L54.1533 6.19722L25.148 38.7274V9.19202V4.69202H20.648H9H4.5V9.19202V98.408V102.908H9H20.648H25.148V98.408V69.4065L54.0434 101.423Z"
+              stroke="#fff"
+              stroke-width="9"
+            />
+          </svg>
         </div>
       </div>
     </div>
