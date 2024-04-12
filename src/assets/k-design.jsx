69f9c71ecc1964fec519214e436f4ba58e95962e
kolamat <matanmidupe004@gmail.com>
2024-04-12 19:27:59 +0100

    On branch main Your branch is ahead of 'origin/main' by 5 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/assets/k-design.jsx

--- src/assets/k-design.jsx
+++ src/assets/k-design.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
-const k-design = () => {
-  return (
-    <div>k-design</div>
-  )
-}
-
-export default k-design
+<svg
+  // className="hover:fill-[#60a5fa] hover:stroke-[#E1986F] transition duration-300 hover:blur-sm"
+  width="250"
+  height="300"
+  viewBox="0 0 94 108"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M54.0434 101.423L55.3836 102.908H57.384H72.616H82.8456L75.9332 95.3673L37.8715 53.8453L75.5678 12.2124L82.377 4.69202H72.232H57.512H55.4954L54.1533 6.19722L25.148 38.7274V9.19202V4.69202H20.648H9H4.5V9.19202V98.408V102.908H9H20.648H25.148V98.408V69.4065L54.0434 101.423Z"
+    stroke="#fff"
+    stroke-width="9"
+  />
+</svg>;
