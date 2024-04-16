97c62820844ecaa771a527cfb6793962fa971718
kolamat <matanmidupe004@gmail.com>
2024-04-16 11:00:46 +0100

    On branch main Your branch is ahead of 'origin/main' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/navbar/Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -27,8 +27,8 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (!document.getElementById("Root").contains(event.target)) {
+    const handleClickOutside = (e) => {
+      if (!document.getElementById("Root").contains(e.target)) {
         // Close your elements here
         setIsOpen(false);
       }
@@ -66,6 +66,8 @@ const Navbar = () => {
 
   return (
     <nav onClick={handleToggle} className={navbar ? "navbar active" : "navbar"}>
+      {isOpen && (
+
       <a href="/" title="HomePage" className="flex">
         <div
           className={`flex items-center justify-between gap-1 sm:ml-6 md:pl-4`}
@@ -90,6 +92,7 @@ const Navbar = () => {
           </h1> */}
         </div>
       </a>
+      
       {/* <menu className="hidden md:flex font-[300] text-md">
         <ul className="hidden md:flex font-[300] text-md">
           {Menus.map((menu) => (
@@ -119,61 +122,61 @@ const Navbar = () => {
       <div onClick={handleNav} className={`block md:hidden cursor-pointer`}>
         {!nav ? <CiMenuKebab size={23} /> : <IoClose size={23} />}
       </div>
-      {isOpen && (
-        <div
-          id="Root"
-          className={
-            nav
-              ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in duration-700"
-              : "fixed left-[-100%]"
-          }
-        >
-          <h1 className={`w-full text-3xl p-3 font-bold text-[#00df9a]`}>
-            <svg
-              className={`hover:fill-[#147bf9] hover:stroke-[#E1986F] transition duration-300`}
-              width="38"
-              height="30"
-              viewBox="0 0 94 108"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M54.0434 101.423L55.3836 102.908H57.384H72.616H82.8456L75.9332 95.3673L37.8715 53.8453L75.5678 12.2124L82.377 4.69202H72.232H57.512H55.4954L54.1533 6.19722L25.148 38.7274V9.19202V4.69202H20.648H9H4.5V9.19202V98.408V102.908H9H20.648H25.148V98.408V69.4065L54.0434 101.423Z"
-                stroke="#fff"
-                stroke-width="9"
-              />
-            </svg>
-          </h1>
+      <div
+        id="Root"
+        className={
+          nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in duration-700"
+            : "fixed left-[-100%]"
+        }
+      >
+        <h1 className={`w-full text-3xl p-3 font-bold text-[#00df9a]`}>
+          <svg
+            className={`hover:fill-[#147bf9] hover:stroke-[#E1986F] transition duration-300`}
+            width="38"
+            height="30"
+            viewBox="0 0 94 108"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M54.0434 101.423L55.3836 102.908H57.384H72.616H82.8456L75.9332 95.3673L37.8715 53.8453L75.5678 12.2124L82.377 4.69202H72.232H57.512H55.4954L54.1533 6.19722L25.148 38.7274V9.19202V4.69202H20.648H9H4.5V9.19202V98.408V102.908H9H20.648H25.148V98.408V69.4065L54.0434 101.423Z"
+              stroke="#fff"
+              stroke-width="9"
+            />
+          </svg>
+        </h1>
 
-          <ul className={`pt-4 uppercase font-[500]`}>
-            <>
-              <a href="#hero">
-                <li className={`${navMobile}`}>Home</li>
-              </a>
-            </>
-            <>
-              <a href="#about">
-                <li className={`${navMobile}`}>About</li>
-              </a>
-            </>
-            <>
-              <a href="#services">
-                <li className={`${navMobile}`}>Services</li>
-              </a>
-            </>
-            <>
-              <a href="#portfolio">
-                <li className={`${navMobile}`}>Portfolio</li>
-              </a>
-            </>
-            <>
-              <a href="#contact">
-                <li className={`${navMobile} border-b-0`}>Contact</li>
-              </a>
-            </>
-          </ul>
-        </div>
-      )}
+        <ul className={`pt-4 uppercase font-[500]`}>
+          <>
+            <a href="#hero">
+              <li className={`${navMobile}`}>Home</li>
+            </a>
+          </>
+          <>
+            <a href="#about">
+              <li className={`${navMobile}`}>About</li>
+            </a>
+          </>
+          <>
+            <a href="#services">
+              <li className={`${navMobile}`}>Services</li>
+            </a>
+          </>
+          <>
+            <a href="#portfolio">
+              <li className={`${navMobile}`}>Portfolio</li>
+            </a>
+          </>
+          <>
+            <a href="#contact">
+              <li className={`${navMobile} border-b-0`}>Contact</li>
+            </a>
+          </>
+        </ul>
+      </div>
+      ) 
+      };
     </nav>
   );
 };
