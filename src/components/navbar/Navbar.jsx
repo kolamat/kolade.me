e7a566731fecce92114ce66430bbda4b2b347b53
kolamat <matanmidupe004@gmail.com>
2024-04-16 14:50:04 +0100

    On branch main Your branch is ahead of 'origin/main' by 10 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/navbar/Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -98,62 +98,62 @@ const Navbar = () => {
         <div onClick={handleNav} className={`block md:hidden cursor-pointer`}>
           {!nav ? <CiMenuKebab size={23} /> : <IoClose size={23} />}
         </div>
-      </button>
 
-      <div
-        id="kolade"
-        className={
-          nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in duration-700"
-            : "fixed left-[-100%]"
-        }
-      >
-        <h1 className={`w-full text-3xl p-3 font-bold text-[#00df9a]`}>
-          <svg
-            className={`hover:fill-[#147bf9] hover:stroke-[#E1986F] transition duration-300`}
-            width="38"
-            height="30"
-            viewBox="0 0 94 108"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M54.0434 101.423L55.3836 102.908H57.384H72.616H82.8456L75.9332 95.3673L37.8715 53.8453L75.5678 12.2124L82.377 4.69202H72.232H57.512H55.4954L54.1533 6.19722L25.148 38.7274V9.19202V4.69202H20.648H9H4.5V9.19202V98.408V102.908H9H20.648H25.148V98.408V69.4065L54.0434 101.423Z"
-              stroke="#fff"
-              stroke-width="9"
-            />
-          </svg>
-        </h1>
-        {isOpen && (
-          <ul className={`pt-4 uppercase font-[500]`}>
-            <>
-              <a href="#hero">
-                <li className={`${navMobile}`}>Home</li>
-              </a>
-            </>
-            <>
-              <a href="#about">
-                <li className={`${navMobile}`}>About</li>
-              </a>
-            </>
-            <>
-              <a href="#services">
-                <li className={`${navMobile}`}>Services</li>
-              </a>
-            </>
-            <>
-              <a href="#portfolio">
-                <li className={`${navMobile}`}>Portfolio</li>
-              </a>
-            </>
-            <>
-              <a href="#contact">
-                <li className={`${navMobile} border-b-0`}>Contact</li>
-              </a>
-            </>
-          </ul>
-        )}
-      </div>
+        <div
+          id="kolade"
+          className={
+            nav
+              ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in duration-700"
+              : "fixed left-[-100%]"
+          }
+        >
+          <h1 className={`w-full text-3xl p-3 font-bold text-[#00df9a]`}>
+            <svg
+              className={`hover:fill-[#147bf9] hover:stroke-[#E1986F] transition duration-300`}
+              width="38"
+              height="30"
+              viewBox="0 0 94 108"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M54.0434 101.423L55.3836 102.908H57.384H72.616H82.8456L75.9332 95.3673L37.8715 53.8453L75.5678 12.2124L82.377 4.69202H72.232H57.512H55.4954L54.1533 6.19722L25.148 38.7274V9.19202V4.69202H20.648H9H4.5V9.19202V98.408V102.908H9H20.648H25.148V98.408V69.4065L54.0434 101.423Z"
+                stroke="#fff"
+                stroke-width="9"
+              />
+            </svg>
+          </h1>
+          {isOpen && (
+            <ul className={`pt-4 uppercase font-[500]`}>
+              <>
+                <a href="#hero">
+                  <li className={`${navMobile}`}>Home</li>
+                </a>
+              </>
+              <>
+                <a href="#about">
+                  <li className={`${navMobile}`}>About</li>
+                </a>
+              </>
+              <>
+                <a href="#services">
+                  <li className={`${navMobile}`}>Services</li>
+                </a>
+              </>
+              <>
+                <a href="#portfolio">
+                  <li className={`${navMobile}`}>Portfolio</li>
+                </a>
+              </>
+              <>
+                <a href="#contact">
+                  <li className={`${navMobile} border-b-0`}>Contact</li>
+                </a>
+              </>
+            </ul>
+          )}
+        </div>
+      </button>
     </nav>
   );
 };
